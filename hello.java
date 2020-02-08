b278f7559a66542a1478a17d96b95d0759cbcfe1
Nirvan <chawlanirvan@gmail.com>
2020-02-08 12:08:00 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Practice1{
+public static void main(String args[]){
+Utility obj = new Utility();
+obj.NamePrinter();
+
+}}
+
+
+class Utility{
+void NamePrinter(){
+System.out.println("Hello From NamePrinter");
+
+}
+}
+
+
